65b8c8c5e052e7f1526608b7dfffb1d0667850d1
Yuway Chen <yuwaychen@Yuways-MacBook-Pro.local>
2024-10-07 23:02:58 +0800

    create the get link api

--- civic_salon/app.py
+++ civic_salon/app.py
@@ -1,5 +1,5 @@
 import os
-from fastapi import FastAPI, Query, Depends
+from fastapi import FastAPI, Query, HTTPException
 from pydantic import BaseModel
 
 from datetime import datetime
@@ -18,6 +18,10 @@ tags_metadata = [
         "name":"get_prompt",
         "description": "Get the user query from front-end."
     },
+    {
+        "name":"get_post_link",
+        "descripton": "Get the post link from the front-end"
+    }
 
 ]
 
@@ -42,6 +46,19 @@ async def ok_endpoint():
 @app.get("/get_prompt", tags=['get_prompt'])
 async def get_prompt(query: str = Query(..., title="User Query", description="User's question")):
     return query
+
+
+@app.get("/get_post_link", tags=['retrieve post link'])
+async def get_post_link(post_id: str = Query(..., description="The ID of the post"), 
+                        post_link: str = Query(..., description="The URL link to the post")):
+    if not post_id or not post_link:
+        raise HTTPException(status_code=400, detail="post_id and post_link are required.")
+    
+    # 回傳前端傳來的 post_id 和 post_link
+    return {
+        "post_id": post_id,
+        "post_link": post_link
+    }
 
 
 
