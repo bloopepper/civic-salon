2de24cb65abb6a94d7b0ced1b032df5a3a1024d1
Yuway Chen <cireidieh@gmail.com>
2024-10-20 22:38:53 +0800

    docker create

--- civic_salon/app.py
+++ civic_salon/app.py
@@ -1,9 +1,14 @@
 import os
+import sys
 from fastapi import FastAPI, Query, HTTPException
 from pydantic import BaseModel
 from datetime import datetime
 from civic_salon.crawl.scraper import UniversalScraper 
 from civic_salon.chain.post_geny import PostHelper
+
+
+# sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 llm_deployment_name = os.getenv('deployment_name')
 
